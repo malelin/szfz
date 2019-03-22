1082c8a16e0cc31d5d2cf62349aed4ebae6618a0
旺苍扛把子 <314231258@qq.com>
2019-03-22 10:24:00 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
+import router from "@/router";
 import store from "./store";
-
+import "@/stylus/index.styl";
 Vue.config.productionTip = false;
 
 new Vue({
