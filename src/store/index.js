1b69b0f4ff864a29398d90190f656806f052f101
旺苍扛把子PC <pc314231258@icloud.com>
2019-04-15 20:06:33 +0800

    vuex数据持久化

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
+//vuex数据持久化
+import createPersistedState from "vuex-persistedstate";
+//根getters
 import getters from "./getters";
-
+// vuex模块
 import app from "./modules/app";
 import layout from "./modules/layout";
 import permission from "./modules/permission";
@@ -11,5 +13,6 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: { app, permission, layout },
-  getters
+  getters,
+  plugins: [createPersistedState()]
 });
