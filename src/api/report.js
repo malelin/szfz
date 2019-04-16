e4410b0f426ba8816dcab4d43e365ba654f2cf33
旺苍扛把子PC <pc314231258@icloud.com>
2019-04-16 10:10:29 +0800

    新增检测报告组件,接口

---
+++ src/api/report.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: 旺苍扛把子
+ * @LastEditors: 旺苍扛把子
+ * @Description: 报告管理模块相关接口
+ * @Date: 2019-04-16 10:08:49
+ * @LastEditTime: 2019-04-16 10:09:21
+ */
---
+++ src/views/Layout/AppMain/DetectReport/DetectReport.vue
@@ -0,0 +1,40 @@
+<!--
+ * @Author: 旺苍扛把子
+ * @LastEditors: 旺苍扛把子
+ * @Description: 检测报告
+ * @Date: 2019-04-16 10:07:40
+ * @LastEditTime: 2019-04-16 10:08:25
+ -->
+<template>
+  <div class="detect-report"></div>
+</template>
+
+<script>
+export default {
+  name: "DetectReport",
+  components: {
+    /* 按需加载组件 */
+    // demo: () => import('@/pages/')
+  },
+  props: {
+    /*  <WelcomeMessage greeting-text="hi"/> */
+    //   'greetingText': {
+    //     type: [String,Number],
+    //     required: true
+    //  }
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+  created() {},
+  mounted() {}
+};
+</script>
+
+<style lang="stylus" scoped>
+.detect-report
+  line-height 1
+</style>
