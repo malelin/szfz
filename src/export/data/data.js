f889c01539c7ee91084f900f16a4ced5793716db
旺苍扛把子PC <pc314231258@icloud.com>
2019-04-24 09:58:57 +0800

    新增报告单机版

---
+++ src/export/data/data.js
@@ -0,0 +1,192 @@
+var reportData = {
+  meta: {
+    rid: 535,
+    objName: "cheatsheet-a5.pdf",
+    repName: "检测报告-cheatsheet-a5.pdf",
+    finishTime: "2019-04-23 09:09:55",
+    detectContent: [
+      {
+        detectContent: "敏感信息分析",
+        levelContent: "失败",
+        detectId: 1
+      }
+    ],
+    level: 4,
+    tags: [],
+    md5: "941c2cde20e1117ad1efe89d88f92460"
+  },
+  basic: {
+    objName: "cheatsheet-a5.pdf",
+    objType: "pdf",
+    size: 262380,
+    uploadTime: "2019-04-23 09:09:54",
+    md5: "941c2cde20e1117ad1efe89d88f92460",
+    sha1: "9221337bbceca9cb090d5d9ffb29dca8c5a446c8",
+    sha256: "c10bc6715507fae63b71ada6ba782f7bca3c7956f34496837f137366feab1328",
+    ssdeep: "6144:iI8/hSeaIMXRjeWsTGcxQO+BJTvLUkdfBi6E:iI8FfW0wTL/df06E"
+  },
+  sensi: {
+    signatures: [
+      {
+        severity: 1,
+        marks: [
+          {
+            size: "0x0000015a",
+            offset: "0x000dfff8",
+            filename: "Server.exe"
+          }
+        ],
+        name: "has_manifest",
+        categories: "Chinese Information",
+        description:
+          "\u00e5\u00ad\u0098\u00e5\u009c\u00a8\u00e6\u00b8\u0085\u00e5\u008d\u0095\u00e6\u0096\u0087\u00e4\u00bb\u00b6"
+      },
+      {
+        severity: 1,
+        marks: [
+          {
+            size: "0x00000468",
+            offset: "0x000f5150",
+            sublanguage: "SUBLANG_CHINESE_SIMPLIFIED",
+            language: "LANG_CHINESE",
+            filename: "Server.exe"
+          },
+          {
+            size: "0x0000003e",
+            offset: "0x000f55b8",
+            sublanguage: "SUBLANG_CHINESE_SIMPLIFIED",
+            language: "LANG_CHINESE",
+            filename: "Server.exe"
+          }
+        ],
+        name: "has_sensitiveLanguage",
+        categories: "Chinese Information",
+        description:
+          "\u00e5\u00ad\u0098\u00e5\u009c\u00a8\u00e6\u0095\u008f\u00e6\u0084\u009f\u00e7\u009a\u0084\u00e5\u008c\u00ba\u00e5\u009f\u009f\u00e6\u0080\u00a7\u00e8\u00af\u00ad\u00e8\u00a8\u0080"
+      },
+      {
+        severity: 1,
+        marks: [
+          {
+            offset: "ea471HH",
+            value: ":$:,:4:<:D:L:T:\\:h:",
+            filename: "Server.exe"
+          }
+        ],
+        name: "has_sensitiveChar",
+        categories: "Chinese Information",
+        description:
+          "\u00e5\u00ad\u0098\u00e5\u009c\u00a8\u00e6\u0095\u008f\u00e6\u0084\u009f\u00e5\u00ad\u0097\u00e7\u00ac\u00a6"
+      },
+      {
+        severity: 1,
+        marks: [
+          {
+            author: "admin"
+          }
+        ],
+        name: "has_author",
+        categories: "Author Information",
+        description:
+          "\u00e5\u00ad\u0098\u00e5\u009c\u00a8\u00e6\u0095\u008f\u00e6\u0084\u009f\u00e5\u00ad\u0097\u00e7\u00ac\u00a6"
+      },
+      {
+        severity: 1,
+        marks: [
+          {
+            create_time: "2019-03-25 20:12:25"
+          }
+        ],
+        name: "has_beijingTime",
+        categories: "BeijingTime Information",
+        description:
+          "\u00e5\u00ad\u0098\u00e5\u009c\u00a8\u00e6\u0095\u008f\u00e6\u0084\u009f\u00e5\u00ad\u0097\u00e7\u00ac\u00a6"
+      },
+      {
+        severity: 1,
+        marks: [
+          {
+            codepage: "947"
+          }
+        ],
+        name: "has_codepage",
+        categories: "CodePage Information",
+        description:
+          "\u00e5\u00ad\u0098\u00e5\u009c\u00a8\u00e6\u0095\u008f\u00e6\u0084\u009f\u00e5\u00ad\u0097\u00e7\u00ac\u00a6"
+      },
+      {
+        severity: 1,
+        marks: [
+          {
+            filename: "ee.exe",
+            pdb_path: "/jds/dsd.sd",
+            offset: "ea471HH"
+          }
+        ],
+        name: "has_pdb",
+        categories: "Chinese Information ",
+        description:
+          "\u00e5\u00ad\u0098\u00e5\u009c\u00a8\u00e6\u0095\u008f\u00e6\u0084\u009f\u00e5\u00ad\u0097\u00e7\u00ac\u00a6"
+      },
+      {
+        severity: 1,
+        marks: [
+          {
+            offset: "2226bcH",
+            value: "??_7QStateMachine@@6B@",
+            filename: "QtCore4.dll"
+          },
+          {
+            offset: "254f90H",
+            value: ".?AVQStateMachine@@",
+            filename: "QtCore4.dll"
+          }
+        ],
+        name: "has_pinyin",
+        categories: "Chinese Information",
+        description:
+          "\u00e5\u00ad\u0098\u00e5\u009c\u00a8\u00e6\u008b\u00bc\u00e9\u009f\u00b3\u00e5\u00ad\u0097\u00e7\u00ac\u00a6"
+      },
+      {
+        severity: 1,
+        marks: [
+          {
+            offset: "2226bcH",
+            value: "??_7QStateMachine@@6B@",
+            filename: "QtCore4.dll"
+          },
+          {
+            offset: "254f90H",
+            value: ".?AVQStateMachine@@",
+            filename: "QtCore4.dll"
+          }
+        ],
+        name: "has_chinese",
+        categories: "Chinese Information",
+        description:
+          "\u00e5\u00ad\u0098\u00e5\u009c\u00a8\u00e6\u008b\u00bc\u00e9\u009f\u00b3\u00e5\u00ad\u0097\u00e7\u00ac\u00a6"
+      },
+      {
+        severity: 1,
+        marks: [
+          {
+            filename: "dds.txt",
+            offset: "254f90H",
+            value: "value",
+            attribute: "attribute"
+          },
+          {
+            filename: "dds.txt",
+            offset: "254f90H",
+            value: "value",
+            attribute: "attribute"
+          }
+        ],
+        name: "has_versionChinese",
+        categories: "Chinese Information",
+        description:
+          "\u00e5\u00ad\u0098\u00e5\u009c\u00a8\u00e6\u008b\u00bc\u00e9\u009f\u00b3\u00e5\u00ad\u0097\u00e7\u00ac\u00a6"
+      }
+    ]
+  }
+};
